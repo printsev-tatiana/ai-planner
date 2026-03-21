d5047879574aefa84d4e0687fb6879711e7c5329
printsev-tatiana <tatianaprintsev@gmail.com>
2026-03-21 18:51:03 +0100

    setup github repo + api basic structure (#2)

---
+++ api/aiplanner/src/main/java/com/tp/aiplanner/AiplannerApplication.java
@@ -0,0 +1,13 @@
+package com.tp.aiplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiplannerApplication.class, args);
+	}
+
+}
