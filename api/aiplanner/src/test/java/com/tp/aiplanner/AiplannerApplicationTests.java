fe92dcb8c24010d8ed36cc64c707432c868757d8
Printsev Tatiana <tatianaprintsev@gmail.com>
2026-03-21 18:42:21 +0100

    setup github repo + api basic structure

---
+++ api/aiplanner/src/test/java/com/tp/aiplanner/AiplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp.aiplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
